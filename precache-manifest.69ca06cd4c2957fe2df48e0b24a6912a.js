cf1a5316ba4c4a147d01e5f2e4cbc28f70bc134a
unknown <cse14009@gmail.com>
2020-07-18 22:51:05 +0600

    Updates

---
+++ precache-manifest.69ca06cd4c2957fe2df48e0b24a6912a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d324e1558e46d6e06784fede5bf32e",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "a1aa5577651e6eaeb5b3",
+    "url": "/WeatherForecast/static/css/2.df967d8d.chunk.css"
+  },
+  {
+    "revision": "de95c533351da34dfe83",
+    "url": "/WeatherForecast/static/css/main.b242b0c1.chunk.css"
+  },
+  {
+    "revision": "a1aa5577651e6eaeb5b3",
+    "url": "/WeatherForecast/static/js/2.bf62daaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherForecast/static/js/2.bf62daaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de95c533351da34dfe83",
+    "url": "/WeatherForecast/static/js/main.04ae19f9.chunk.js"
+  },
+  {
+    "revision": "37e5b636571cf3968910",
+    "url": "/WeatherForecast/static/js/runtime-main.74dbc6fa.js"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/WeatherForecast/static/media/01d.3077681e.svg"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/WeatherForecast/static/media/01n.3077681e.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/WeatherForecast/static/media/02d.144d9a47.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/WeatherForecast/static/media/02n.144d9a47.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/WeatherForecast/static/media/03d.8d2c0b2d.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/WeatherForecast/static/media/03n.8d2c0b2d.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/WeatherForecast/static/media/04d.5a81a676.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/WeatherForecast/static/media/04n.5a81a676.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/WeatherForecast/static/media/09d.a92066b1.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/WeatherForecast/static/media/09n.a92066b1.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/WeatherForecast/static/media/10d.810f7538.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/WeatherForecast/static/media/10n.810f7538.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/WeatherForecast/static/media/11d.0fc0affe.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/WeatherForecast/static/media/11n.0fc0affe.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/WeatherForecast/static/media/13d.dc58368f.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/WeatherForecast/static/media/13n.dc58368f.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/WeatherForecast/static/media/50d.1186186b.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/WeatherForecast/static/media/50n.1186186b.svg"
+  },
+  {
+    "revision": "961673a4bf41f578a34502a91718adf0",
+    "url": "/WeatherForecast/static/media/cloudy_moon.961673a4.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/WeatherForecast/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/WeatherForecast/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/WeatherForecast/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/WeatherForecast/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/WeatherForecast/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
